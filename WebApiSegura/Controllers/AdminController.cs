cb775a05015863abbd46dbf07346bc1d5316acf4
Santimacnet <santimacnet@live.com>
2018-10-15 21:39:15 +0200

    Add Admin controller to project

--- WebApiSegura/Controllers/AdminController.cs
+++ WebApiSegura/Controllers/AdminController.cs
@@ -7,7 +7,25 @@ using System.Web.Http;
 
 namespace WebApiSegura.Controllers
 {
+    /// <summary>
+    /// admin controller class for testing security token with role admin
+    /// </summary>
+    [Authorize]
+    [RoutePrefix("api/admin")]
     public class AdminController : ApiController
     {
+        [HttpGet]
+        public IHttpActionResult GetId(int id)
+        {
+            var adminFake = "admin-fake: " + id;
+            return Ok(adminFake);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var adminList = new string[] { "admin-1", "admin-2", "admin-3" };
+            return Ok(adminList);
+        }
     }
 }
